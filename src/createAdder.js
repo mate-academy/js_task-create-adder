e7eb4d3d4478325071d60a7e138fdb8b021a8c93
RavenVol <r.volianiuk@gmail.com>
2019-06-24 19:32:34 +0300

    Create Adder Closure - Solved by Raven

--- src/createAdder.js
+++ src/createAdder.js
@@ -23,6 +23,11 @@
  */
 function createAdder(initialValue = 0) {
   // write code here
+  let sum = initialValue;
+  return (...args) => {
+    args.forEach((item) => { sum += item; });
+    return sum;
+  };
 }
 
 module.exports = createAdder;
