bfc4eeb672b8e711ff569bca223903bd547c5d58
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-23 16:30:25 +0300

    'Solution'

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,17 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let currentValue = initialValue;
+
+  return function(...args) {
+    let sum = 0;
+
+    [...args].forEach(function(item) {
+      sum += item;
+    });
+    currentValue += sum;
+    return currentValue;
+  };
 }
 
 module.exports = createAdder;
