17ee7da91f9a870b36bab520810228341276c746
Volodsher <volodsher@gmail.com>
2019-08-23 17:24:37 +0300

    after refactoring

--- src/createAdder.js
+++ src/createAdder.js
@@ -1,18 +1,14 @@
 'use strict';
 
 function createAdder(initialValue = 0) {
-  const values = [];
-  values.push(initialValue);
+  let values = initialValue;
 
-  return function nowValue(...value) {
-    for (const element of value) {
-      values.push(element);
-    };
-    let sum = 0;
-    for (const el of values) {
-      sum = sum + el;
-    }
-    return sum;
+  return function(...value) {
+    value.forEach(function(element) {
+      values += element;
+    });
+
+    return values;
   };
 }
 
