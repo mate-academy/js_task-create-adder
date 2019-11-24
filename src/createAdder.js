ad8ea682d9dba335e47b508ff6dd01c92cd9d033
Alex Hodovas <o.hodovas@gmail.com>
2019-11-24 18:25:27 +0200

    solution

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,8 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let sum = initialValue;
+  return (...args) => sum = args.reduce( (result, elem) => result += elem, sum);  // eslint-disable-line
 }
 
 module.exports = createAdder;
