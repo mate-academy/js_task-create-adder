7c7fbc6a169705228a0e406d991f5afd55073b40
vadim-os <osinsky_vadim@yahoo.com>
2019-11-25 13:46:19 +0200

    solution

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,15 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let sum = initialValue;
+
+  return function adder() {
+    sum = sum + [...arguments].reduce(
+      (total, item) => total + item, 0
+    );
+
+    return sum;
+  };
 }
 
 module.exports = createAdder;
