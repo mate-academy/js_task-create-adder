7160a7a3217e3d6a1ca161434a5f8fec3de9b469
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-03 15:47:42 +0200

    Create Adder

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,15 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let sum = initialValue;
+
+  return function(...numbers) {
+    for (const item of numbers) {
+      sum += item;
+    }
+
+    return sum;
+  };
 }
 
 module.exports = createAdder;
