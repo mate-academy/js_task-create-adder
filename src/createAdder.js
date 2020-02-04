2af46f629a53825f11e262e792afe95757f131a4
ded-git <sakarailko@gmail.com>
2020-02-04 16:49:33 +0200

    rest

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,15 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let result = initialValue;
+
+  return function(args) {
+    [...arguments].forEach(element => {
+      result += element;
+    });
+
+    return result;
+  };
 }
 
 module.exports = createAdder;
