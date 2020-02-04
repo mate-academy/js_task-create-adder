9545f832a5cd72d624e91e0e998c7c273667f605
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-02-04 13:23:39 +0200

    create adder

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,16 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  // eslint-disable-next-line no-unused-vars
+  let sum = initialValue;
+
+  return function(...args) {
+    const result = [...args].reduce((a, b) => a + b, 0);
+
+    sum += result;
+
+    return sum;
+  };
 }
 
 module.exports = createAdder;
