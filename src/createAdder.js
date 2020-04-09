5753199c1da9408a01505e064f4928bc35b636e4
Yevhenii <DrozhzhynY>
2020-04-09 17:35:22 +0300

    solution

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,13 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let result = initialValue;
+
+  return function(...args) {
+    result = result + [...args].reduce((a, b) => a + b, 0);
+
+    return result;
+  };
 }
 
 module.exports = createAdder;
