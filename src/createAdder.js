831756fff7947ac619ad952a5878f8612a292a4c
Vitalii Korol <vitaliikorol@github.com>
2020-04-10 09:36:13 +0300

    solution

--- src/createAdder.js
+++ src/createAdder.js
@@ -22,7 +22,16 @@
  * @return {number}
  */
 function createAdder(initialValue = 0) {
-  // write code here
+  let value = initialValue;
+
+  return (...numbers) => {
+    if (numbers.length === 0) {
+      return value;
+    }
+    value += numbers.reduce((a, b) => a + b);
+
+    return value;
+  };
 }
 
 module.exports = createAdder;
