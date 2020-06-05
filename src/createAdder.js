952c9eab4c5478638181183b0f1bbc45efbf9824
VlaDinter <vladyslavdinter@gmail.com>
2020-06-05 09:14:00 +0300

    Solution

--- src/createAdder.js
+++ src/createAdder.js
@@ -1,26 +1,13 @@
 'use strict';
 
-/**
- * Write a function (factory) creating a function (device) returning the sum of
- * initialValue and all the arguments of current and all previous calls. By
- * default initial value is 0.
- *
- * For example:
- *
- * const adder1 = createAdder()
- * console.log(adder1()); // 0
- * console.log(adder1(10, 20)); // 30 = 0 + (10 + 20)
- * console.log(adder1(30, 40)); // 100 = 0 + (10 + 20) + (30 + 40)
- *
- * const adder2 = createAdder(100);
- * console.log(adder2(10)); // 110 = 100 + 10
- * console.log(adder2(20, 30, 40)); 200 = 100 + 10 + (20 + 30 + 40)
- * console.log(adder2()); 200 = the same as previous
- *
- * @param {number} initialValue
- */
 function createAdder(initialValue = 0) {
-  // write code here
+  let numbersSum = initialValue;
+
+  return (...numbers) => {
+    numbersSum = numbers.reduce((a, number) => a + number, numbersSum);
+
+    return numbersSum;
+  };
 }
 
 module.exports = createAdder;
